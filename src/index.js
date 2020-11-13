e3237c407a6fea8e997bc201d239a91f88853daa
Kushch Oleg <o.kushch@netbox.ru>
2020-11-13 11:06:15 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Tabele from './components/Table/Table.component';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
